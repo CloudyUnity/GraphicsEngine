eff6110d5cb232f4a6a8f216f61c528d520595d5
CloudyUnity <finn.wright5@gmail.com>
2024-04-14 20:32:24 +0100

    7 hours of looking at normal vectors. Finally figured it out, was the fucking world matrix scale aghhhh

--- ModelLineListClass.cpp
+++ ModelLineListClass.cpp
@@ -3,8 +3,6 @@
 bool ModelLineListClass::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext, vector<XMFLOAT3> points)
 {
 	m_points = points;
-	m_vertexCount = points.size();
-	m_indexCount = m_vertexCount;
 
 	m_boundingRadius = 0;
 	for (int i = 0; i < points.size(); i++)
@@ -33,33 +31,38 @@ void ModelLineListClass::RenderBuffers(ID3D11DeviceContext* deviceContext)
 }
 
 bool ModelLineListClass::InitializeBuffers(ID3D11Device* device)
-{
-	LineVertexType* vertices;
+{	
 	unsigned long* indices;
 	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
 	D3D11_SUBRESOURCE_DATA vertexData, indexData;
 	HRESULT result;
 
-	vertices = new LineVertexType[m_vertexCount];
+	m_vertexCount = 50;
+	m_indexCount = m_vertexCount;
+
+	m_vertices = new LineVertexType[m_vertexCount];
+	memset(m_vertices, 0, (sizeof(LineVertexType) * m_vertexCount));
+
 	indices = new unsigned long[m_indexCount];
 
 	for (int i = 0; i < m_vertexCount; i++)
 	{
-		vertices[i].position = m_points[i];
+		if (i < m_points.size())
+			m_vertices[i].position = m_points[i];
 
 		indices[i] = i;
 	}
 
 	// Set up the description of the static vertex buffer.
-	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
 	vertexBufferDesc.ByteWidth = sizeof(LineVertexType) * m_vertexCount;
 	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 	vertexBufferDesc.MiscFlags = 0;
 	vertexBufferDesc.StructureByteStride = 0;
 
 	// Give the subresource structure a pointer to the vertex data.
-	vertexData.pSysMem = vertices;
+	vertexData.pSysMem = m_vertices;
 	vertexData.SysMemPitch = 0;
 	vertexData.SysMemSlicePitch = 0;
 
@@ -86,11 +89,42 @@ bool ModelLineListClass::InitializeBuffers(ID3D11Device* device)
 	if (FAILED(result))
 		return false;
 
-	delete[] vertices;
-	vertices = 0;
-
 	delete[] indices;
 	indices = 0;
 
 	return true;
+}
+
+bool ModelLineListClass::UpdateBuffers(ID3D11DeviceContext* deviceContext, vector<XMFLOAT3> points)
+{
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	LineVertexType* verticesPtr;
+
+	m_points = points;
+	m_vertexCount = points.size();
+	m_indexCount = m_vertexCount;
+
+	m_boundingRadius = 0;
+	for (int i = 0; i < points.size(); i++)
+	{
+		float dist = sqrt(points[i].x * points[i].x + points[i].y * points[i].y + points[i].z * points[i].z);
+		if (dist > m_boundingRadius)
+			m_boundingRadius = dist;
+	}
+
+	memset(m_vertices, 0, (sizeof(LineVertexType) * m_vertexCount));
+
+	for (int i = 0; i < m_points.size(); i++)
+	{
+		m_vertices[i].position = m_points[i];
+	}
+
+	HRESULT result = deviceContext->Map(m_vertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	if (FAILED(result))
+		return false;
+
+	verticesPtr = (LineVertexType*)mappedResource.pData;
+
+	memcpy(verticesPtr, (void*)m_vertices, (sizeof(LineVertexType) * m_vertexCount));
+	deviceContext->Unmap(m_vertexBuffer, 0);
 }
--- SceneOceanClass.cpp
+++ SceneOceanClass.cpp
@@ -1,5 +1,4 @@
 #include "SceneOceanClass.h"
-#include "ModelLineListClass.h"
 
 SceneOceanClass::SceneOceanClass()
 {
@@ -10,11 +9,24 @@ bool SceneOceanClass::InitializeScene(HWND hwnd)
 	bool result;
 
 	m_Camera = new CameraClass;
-	m_Camera->SetPosition(0.0f, 0.0f, -5.0f);
+	m_Camera->SetPosition(0.0f, 5.0f, -20.0f);
 	m_Camera->Initialize2DView();
+
+	float dirLightX = 0.0;
+	float dirLightY = -0.5f;
+	float dirLightZ = 1.0;
+
+	m_DirLight = new LightClass();
+	m_DirLight->SetAmbientColor(0.2f, 0.2f, 0.3f, 1.0f);
+	m_DirLight->SetDiffuseColor(0.8f, 1.0f, 1.0f, 1.0f);
+	m_DirLight->SetDirection(dirLightX, dirLightY, dirLightZ);
+	m_DirLight->SetSpecularColor(1.0f, 1.0f, 1.0f, 1.0f);
+	m_DirLight->SetSpecularPower(32.0f);
 
 	ID3D11Device* device = m_Direct3D->GetDevice();
 	ID3D11DeviceContext* context = m_Direct3D->GetDeviceContext();
+
+	// SHADERS
 
 	bool clampSamplerMode = true;
 	ShaderClass* shaderFractal = 0, * shaderLine = 0;
@@ -27,19 +39,25 @@ bool SceneOceanClass::InitializeScene(HWND hwnd)
 	if (!result)
 		return false;
 
+	// TEXSETS
+
 	TextureSetClass* texSetOcean;
 	CreateTexSet(&texSetOcean);
 	texSetOcean->Add(device, context, "../GraphicsEngine/Data/Celeste.tga");
 
+	// MODELS
+
 	vector<XMFLOAT3> debugLinePoints;
-	debugLinePoints.push_back(XMFLOAT3(0, 1000, 0));
-	debugLinePoints.push_back(XMFLOAT3(0, -1000, 0));
+	debugLinePoints.push_back(XMFLOAT3(dirLightX * 1000, dirLightY * 1000, dirLightZ * 1000));
+	debugLinePoints.push_back(XMFLOAT3(dirLightX * -1000, dirLightY * -1000, dirLightZ * -1000));
 
 	ModelClass* modelPlane = 0, * modelCube = 0;
-	ModelLineListClass* modelLine = 0;
 	result = 
 		CreateModel(hwnd, &modelPlane, "../GraphicsEngine/Models/Plane.txt") &&
-		CreateModel(hwnd, &modelLine, debugLinePoints) &&
+		CreateModel(hwnd, &m_debugLineLight, debugLinePoints) &&
+		CreateModel(hwnd, &m_debugLineNormal, debugLinePoints) &&
+		CreateModel(hwnd, &m_debugLineTangent, debugLinePoints) &&
+		CreateModel(hwnd, &m_debugLineBinormal, debugLinePoints) &&
 		CreateModel(hwnd, &modelCube, "../GraphicsEngine/Models/Cube.txt");
 	if (!result)
 		return false;
@@ -49,29 +67,67 @@ bool SceneOceanClass::InitializeScene(HWND hwnd)
 	modelPlane->SetPrimitiveControlPointPatchList(usingTri ? 3 : 4);
 	modelCube->SetPrimitiveControlPointPatchList(3);
 
+	// GOS
+
 	GameObjectClass* goTest;
 	// CreateGameObject(modelCube, shaderOceanTri, texSetOcean, false, "Test", &goTest);
 
-	GameObjectClass* debugLineGo;
-	CreateGameObject(modelLine, shaderLine, nullptr, false, "Line", &debugLineGo);
+	GameObjectClass* debugLineGo, * debugLineTangent, * debugLineNormal, * debugLineBinormal;
+	CreateGameObject(m_debugLineLight, shaderLine, nullptr, false, "LineYellow", &debugLineGo);
+	CreateGameObject(m_debugLineNormal, shaderLine, nullptr, false, "LineBlue", &debugLineNormal);
+	CreateGameObject(m_debugLineTangent, shaderLine, nullptr, false, "LineRed", &debugLineTangent);
+	CreateGameObject(m_debugLineBinormal, shaderLine, nullptr, false, "LineGreen", &debugLineBinormal);
 
 	CreateGameObject(modelPlane, (usingTri ? shaderOceanTri : shaderOcean), texSetOcean, false, "Ocean", &m_GoOcean);
-	m_GoOcean->SetScale(20, 1.5f, 20);
-	m_GoOcean->SetPosition(0, -5, 10);
+	m_GoOcean->SetScale(20, 1, 20);
+	m_GoOcean->SetPosition(0, 0, 0);
 	m_GoOcean->SetBackCulling(false);
-
-	m_DirLight = new LightClass();
-	m_DirLight->SetAmbientColor(0.2f, 0.2f, 0.3f, 1.0f);
-	m_DirLight->SetDiffuseColor(0.8f, 1.0f, 1.0f, 1.0f);
-	m_DirLight->SetDirection(1, -0.5f, 0);
-	m_DirLight->SetSpecularColor(1.0f, 1.0f, 1.0f, 1.0f);
-	m_DirLight->SetSpecularPower(32.0f);
 
 	return true;
 }
 
-bool SceneOceanClass::Frame(InputClass*, float frameTime)
+bool SceneOceanClass::Frame(InputClass* input, float frameTime)
 {
+	XMFLOAT2 D = XMFLOAT2(1, 1);
+	float ddx = 0, ddz = 0;
+	float posY = 0;
+
+	for (int i = 0; i < SIN_COUNT; i++)
+	{
+		float a = m_oceanSines[i].x;
+		float p = m_oceanSines[i].y;
+		float freq = m_oceanSines[i].z;		
+
+		posY += a * sin(p * m_time);
+
+		ddx += a * freq * D.x * cos(p * m_time);
+		ddz += a * freq * D.y * cos(p * m_time);
+	}
+
+	XMFLOAT3 tangent = XMFLOAT3(1, 0, ddz);
+	XMFLOAT3 binormal = XMFLOAT3(0, 1, ddx);
+	XMFLOAT3 normal = XMFLOAT3(-ddx, 1, -ddz);
+	float mag = sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+	normal.x /= mag;
+	normal.y /= mag;
+	normal.z /= mag;
+
+	vector<XMFLOAT3> points;
+
+	points.push_back(XMFLOAT3(tangent.x * 1000, tangent.y * 1000 + posY, tangent.z * 1000));
+	points.push_back(XMFLOAT3(0, posY, 0));
+	m_debugLineTangent->UpdateBuffers(m_Direct3D->GetDeviceContext(), points);
+	points.clear();
+
+	points.push_back(XMFLOAT3(binormal.x * 1000, binormal.y * 1000 + posY, binormal.z * 1000));
+	points.push_back(XMFLOAT3(0, posY, 0));
+	m_debugLineBinormal->UpdateBuffers(m_Direct3D->GetDeviceContext(), points);
+	points.clear();
+
+	points.push_back(XMFLOAT3(normal.x * 1000, normal.y * 1000 + posY, normal.z * 1000));
+	points.push_back(XMFLOAT3(0, posY, 0));
+	m_debugLineNormal->UpdateBuffers(m_Direct3D->GetDeviceContext(), points);
+
 	return true;
 }
 
@@ -91,6 +147,12 @@ void SceneOceanClass::SetParameters(ShaderClass::ShaderParameters* params)
 	params->light.specularPower = m_DirLight->GetSpecularPower();
 
 	params->camera.cameraPosition = m_Camera->GetPosition();
+
+	m_time = params->utils.time;
+	// m_time = 28.447;
+
+	for (int i = 0; i < SIN_COUNT; i++)
+		m_oceanSines[i] = params->oceanSine.ampPhaseFreq[i];
 }
 
 void SceneOceanClass::OnSwitchTo()
