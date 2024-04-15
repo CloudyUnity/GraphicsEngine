7262d35dc391c71aed5b51bab34f1e0745395c29
Fiona Wright <fiona.wgt@gmail.com>
2024-04-16 00:39:07 +0100

    Fixed post processing

--- Settings.cpp
+++ Settings.cpp
@@ -21,6 +21,7 @@ Settings::Settings(int index)
 
 	m_performanceData4.ReflectionEnabled = false;
 	m_performanceData4.ShadowsEnabled = false;
+	m_performanceData4.SkyboxEnabled = false;
 
 	m_fogExhibitData5.FogEnabled = true;
 	m_fogExhibitData5.SkyboxEnabled = false;
